fc9f71afe16f80a1fca036ac53db7db95f38c505
corybrunson <cornelioid@gmail.com>
2020-10-23 17:39:26 -0400

    doc(roxygen2): markdown syntax in simplex-tree.R

--- R/simplex-tree.R
+++ R/simplex-tree.R
@@ -1,54 +1,56 @@
+#' @md
 #' @name simplex_tree
 #' @aliases SimplexTree
 #' @title Simplex Tree
 #' @description Simplex tree class exposed as an Rcpp Module.
 #' @docType class 
 #' @details A simplex tree is an ordered trie-like structure specialized for storing and doing general computation 
-#' simplicial complexes. Here is figure of a simplex tree, taken from the original paper (see 1): \cr  
+#' simplicial complexes. Here is figure of a simplex tree, taken from the original paper (see Boissonnat and Maria, 2014):
+#' 
 #' \if{html}{\figure{simplextree.png}{options: width="80\%" alt="Figure: simplextree.png"}}
 #' \if{latex}{\figure{simplextree.pdf}{options: width=12cm}}
-#' \cr 
+#' 
 #' The current implementation provides a subset of the functionality described in the paper.
 #' 
-#' @field n_simplices A vector, where each index k denotes the number (k-1)-simplices.
+#' @field n_simplices A vector, where each index \eqn{k} denotes the number \eqn{(k-1)}-simplices.
 #' @field dimension The dimension of the simplicial complex.
 #' @section Properties:
 #' Properties are actively bound shortcuts to various methods of the simplex tree that may be thought of as fields. 
 #' Unlike fields, however, properties are not explicitly stored: they are generated on access. 
 #' \describe{
-#'     \item{$\code{id_policy}}{ The policy used to generate new vertex ids. May be assigned "compressed" or "unique". See \code{\link{generate_ids}}. }
-#'     \item{$\code{vertices}}{ The 0-simplices of the simplicial complex, as a matrix. }
-#'     \item{$\code{edges}}{ The 1-simplices of the simplicial complex, as a matrix. }
-#'     \item{$\code{triangles}}{ The 2-simplices of the simplicial complex, as a matrix. }
-#'     \item{$\code{quads}}{ The 3-simplices of the simplicial complex, as a matrix. }
-#'     \item{$\code{connected_components}}{ The connected components of the simplicial complex. }
+#'     \item{`$id_policy`}{ The policy used to generate new vertex ids. May be assigned `"compressed"` or `"unique"`. See [generate_ids()]. }
+#'     \item{`$vertices`}{ The 0-simplices of the simplicial complex, as a matrix. }
+#'     \item{`$edges`}{ The 1-simplices of the simplicial complex, as a matrix. }
+#'     \item{`$triangles`}{ The 2-simplices of the simplicial complex, as a matrix. }
+#'     \item{`$quads`}{ The 3-simplices of the simplicial complex, as a matrix. }
+#'     \item{`$connected_components`}{ The connected components of the simplicial complex. }
 #' } 
 #' @section Methods: 
 #' \describe{
-#'     \item{$\code{as_XPtr}}{ Creates an external pointer. }
-#'     \item{$\code{clear}}{ Clears the simplex tree. }
-#'     \item{$\code{\link{generate_ids}}}{ Generates new vertex ids according to the set policy. }
-#'     \item{$\code{\link{degree}}}{ Returns the degree of each given vertex. }
-#'     \item{$\code{\link{adjacent}}}{ Returns vertices adjacent to a given vertex. }
-#'     \item{$\code{\link{insert}}}{ Inserts a simplex into the trie. }
-#'     \item{$\code{\link{remove}}}{ Removes a simplex from the trie. }
-#'     \item{$\code{\link{find}}}{ Returns whether a simplex exists in the trie. }
-#'     \item{$\code{\link{collapse}}}{ Performs an elementary collapse. }
-#'     \item{$\code{\link{contract}}}{ Performs an edge contraction. }
-#'     \item{$\code{\link{expand}}}{ Performs an k-expansion. }
-#'     \item{$\code{\link{traverse}}}{ Traverses a subset of the simplex tree, applying a function to each simplex. }
-#'     \item{$\code{\link{ltraverse}}}{ Traverses a subset of the simplex tree, applying a function to each simplex and returning the result as a list. }
-#'     \item{$\code{\link{is_face}}}{ Checks for faces. }
-#'     \item{$\code{\link{is_tree}}}{ Checks if the simplicial complex is a tree. }
-#'     \item{$\code{as_list}}{ Converts the simplicial complex to a list. }
-#'     \item{$\code{as_adjacency_matrix}}{ Converts the 1-skeleton to an adjacency matrix. }
-#'     \item{$\code{as_adjacency_list}}{ Converts the 1-skeleton to an adjacency list. }
-#'     \item{$\code{as_edgelist}}{ Converts the 1-skeleton to an edgelist. }
+#'     \item{`$as_XPtr()`}{ Creates an external pointer. }
+#'     \item{`$`[generate_ids()]}{ Generates new vertex ids according to the set policy. }
+#'     \item{`$`[insert()]}{ Inserts a simplex into the trie. }
+#'     \item{`$`[remove()]}{ Removes a simplex from the trie. }
+#'     \item{`$`[find()]}{ Returns whether a simplex exists in the trie. }
+#'     \item{`$`[degree()]}{ Returns the degree of each given vertex. }
+#'     \item{`$`[adjacent()]}{ Returns vertices adjacent to a given vertex. }
+#'     \item{`$`[clear()]}{ Clears the simplex tree. }
+#'     \item{`$`[expand()]}{ Performs an k-expansion. }
+#'     \item{`$`[collapse()]}{ Performs an elementary collapse. }
+#'     \item{`$`[contract()]}{ Performs an edge contraction. }
+#'     \item{`$`[traverse()]}{ Traverses a subset of the simplex tree, applying a function to each simplex. }
+#'     \item{`$`[ltraverse()]}{ Traverses a subset of the simplex tree, applying a function to each simplex and returning the result as a list. }
+#'     \item{`$`[is_face()]}{ Checks for faces. }
+#'     \item{`$`[is_tree()]}{ Checks if the simplicial complex is a tree. }
+#'     \item{`$as_list()`}{ Converts the simplicial complex to a list. }
+#'     \item{`$as_adjacency_matrix()`}{ Converts the 1-skeleton to an adjacency matrix. }
+#'     \item{`$as_adjacency_list()`}{ Converts the 1-skeleton to an adjacency list. }
+#'     \item{`$as_edgelist()`}{ Converts the 1-skeleton to an edgelist. }
 #' }
 #' @author Matt Piekenbrock
 #' @import methods
-#' @param simplices optional simplices to initialize the simplex tree with. See \code{\link{insert}}.
-#' @return A queryable simplex tree, as a \code{Rcpp_SimplexTree} object (Rcpp module). 
+#' @param simplices optional simplices to initialize the simplex tree with. See [insert()].
+#' @return A queryable simplex tree, as a `Rcpp_SimplexTree` object (Rcpp module). 
 #' @references Boissonnat, Jean-Daniel, and Clement Maria. "The simplex tree: An efficient data structure for general simplicial complexes." Algorithmica 70.3 (2014): 406-427.
 #' @examples
 #' ## Recreating simplex tree from figure. 
@@ -98,6 +100,7 @@ simplex_tree <- function(simplices = NULL){
 
 # ---- generate_ids ----
 
+#' @md
 #' @name generate_ids
 #' @aliases id_policy
 #' @title Generate vertex ids
@@ -108,9 +111,8 @@ simplex_tree <- function(simplices = NULL){
 #' new 0-simplices, e.g. insertions, contractions, collapses, etc. There are two 'policies' which designate 
 #' the generating mechanism of these ids: 'compressed' and 'unique'. 'compressed' generates vertex ids 
 #' sequentially, starting at 0. 'unique' tracks an incremental internal counter, which is updated on every 
-#' call to \code{generate_ids}. The new ids under the 'unique' policy generates the first sequential \code{n} 
-#' ids that are strictly greater  \code{max}(\emph{counter}, \emph{max vertex id}). \cr
-#' \cr
+#' call to `generate_ids()`. The new ids under the 'unique' policy generates the first sequential `n` 
+#' ids that are strictly greater `max(`\emph{counter}`, `\emph{max vertex id}`)`.
 #' 
 #' @examples 
 #' st <- simplex_tree()
@@ -138,16 +140,17 @@ generate_ids <- function(st, n){
 
 # ---- insert ----
 
+#' @md
 #' @name insert
 #' @title Insert simplices
 #' @description Inserts simplices into the simplex tree. Individual simplices are specified as vectors, and a set of simplices as a list of vectors. 
 #' @param st a simplex tree.  
 #' @param simplices simplices to insert, either as a vector, a list of vectors, or a column-matrix. See details. 
 #' @details This function allows insertion of arbitrary order simplices. If the simplex already exists in the tree, 
-#' no insertion is made, and the tree is not modified. \code{simplex} is sorted before traversing the trie. 
-#' Faces of \code{simplex} not in the simplex tree are inserted as needed. \cr
-#' \cr
-#' If \code{simplices} is a vector, it's assumed to be a simplex. If a list, its assumed each element in the list 
+#' no insertion is made, and the tree is not modified. `simplices` is sorted before traversing the trie. 
+#' Faces of `simplices` not in the simplex tree are inserted as needed.
+#' 
+#' If `simplices` is a vector, it's assumed to be a simplex. If a list, its assumed each element in the list 
 #' represents a simplex (as vectors). If the simplices to insert are all of the same dimension, you can also 
 #' optionally use a matrix, where each column is assumed to be a simplex. 
 #' @family simplex-level operations
@@ -165,16 +168,17 @@ insert <- function(st, simplices){
 
 # ---- remove ----
 
+#' @md
 #' @name remove
 #' @title Remove simplices
 #' @description Removes simplices from the simplex tree. Individual simplices are specified as vectors, and a set of simplices as a list of vectors. 
 #' @param st a simplex tree.  
 #' @param simplices simplices to insert, either as a vector, a list of vectors, or a column-matrix. See details. 
-#' @details This function allows removal of a arbitrary order simplices. If \code{simplex} already exists in the tree, 
-#' it is removed, otherwise the tree is not modified. \code{simplex} is sorted before traversing the trie.
-#' Cofaces of \code{simplex} are also removed. \cr
-#' \cr
-#' If \code{simplices} is a vector, it's assumed to be a simplex. If a list, its assumed each element in the list 
+#' @details This function allows removal of a arbitrary order simplices. If `simplices` already exists in the tree, 
+#' it is removed, otherwise the tree is not modified. `simplices` is sorted before traversing the trie.
+#' Cofaces of `simplices` are also removed.
+#' 
+#' If `simplices` is a vector, it's assumed to be a simplex. If a list, its assumed each element in the list 
 #' represents a simplex (as vectors). If the simplices to insert are all of the same dimension, you can also 
 #' optionally use a matrix, where each column is assumed to be a simplex. 
 #' @family simplex-level operations
@@ -187,6 +191,7 @@ remove <- function(st, simplices){
 
 # ---- find ----
 
+#' @md
 #' @name find
 #' @title Find simplices
 #' @description Returns whether supplied simplices exist in the complex.  
@@ -194,14 +199,14 @@ remove <- function(st, simplices){
 #' @param simplices simplices to insert, either as a vector, a list of vectors, or a column-matrix. See details. 
 #' @section Usage:
 #' st %>% find(simplices)
-#' @details Traverses the simplex tree looking for \code{simplex}, returning whether or not it exists.
-#' \code{simplex} can be specified as vector to represent a single simplex, and a list to represent a set of simplices. 
-#' Each \code{simplex} is sorted before traversing the trie. \cr
-#' \cr
-#' If \code{simplices} is a vector, it's assumed to be a simplex. If a list, its assumed each element in the list 
+#' @details Traverses the simplex tree looking for `simplices`, returning whether or not it exists.
+#' `simplices` can be specified as vector to represent a single simplex, and a list to represent a set of simplices. 
+#' Each `simplices` is sorted before traversing the trie.
+#' 
+#' If `simplices` is a vector, it's assumed to be a simplex. If a list, its assumed each element in the list 
 #' represents a simplex (as vectors). If the simplices to insert are all of the same dimension, you can also 
 #' optionally use a matrix, where each column is assumed to be a simplex. 
-#' @return boolean indicating whether or not \code{simplex} exists in the tree. 
+#' @return boolean indicating whether or not `simplices` exists in the tree. 
 #' @family simplex-level operations
 #' @export
 find <- function(st, simplices){
@@ -211,14 +216,15 @@ find <- function(st, simplices){
 
 # ---- reindex ----
 
+#' @md
 #' @name reindex 
 #' @title Reindex vertex ids
 #' @description This function allows one to 'reorder' or 'reindex' vertex ids.  
 #' @param st a simplex tree. 
 #' @param ids vector of new vertex ids. See details. 
-#' @details The \code{ids} parameter must be a sorted integer vector of new ids with length matching the 
-#' number of vertices. The simplex tree is modified to replace the vertex label at index \code{i} with 
-#' \code{ids}[i]. See examples.
+#' @details The `ids` parameter must be a sorted integer vector of new ids with length matching the 
+#' number of vertices. The simplex tree is modified to replace the vertex label at index `i` with 
+#' `ids[i]`. See examples.
 #' @examples 
 #' st <- simplex_tree()
 #' st %>% insert(1:3) %>% print_simplices("tree")
